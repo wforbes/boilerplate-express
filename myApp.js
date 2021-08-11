e7c6e85157c57227d6ce548bf347a1535527190d
Will Forbes <wforbes87@gmail.com>
2021-08-11 10:44:23 -0700

    challenge 7 - Realizing I should have been working in myApp.js...

--- myApp.js
+++ myApp.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
-console.log('Hello world');
+app.use("/public", express.static(__dirname + '/public'))
+	.get("/", function(req, res) {
+		res.sendFile(__dirname + "/views/index.html");
+	})
+	.get("/json", function(req, res) {
+		res.json({
+			"message": (process.env.MESSAGE_STYLE === 'uppercase') ? "HELLO JSON" : "Hello json"
+		});
+	})
+	.get("/now", function(req, res, next) {
+		req.time = new Date().toString();
+		next();
+	}, function(req, res) {
+		// check if /now route has a middleware before the handler
+		// var stack = (myApp.parent._router && myApp.parent._router.stack) || [];
+		// var nowRoute = stack.filter((l) => {
+		// 	if (l.route) {
+		// 		return l.route.path === '/now';
+		// 	}
+		// 	return false;
+		// });
+		// myApp._router = myApp.parent._router;
+		res.json({
+			time: req.time
+		});
+	});
 
 
 
--- server.js
+++ server.js
@@ -3,8 +3,7 @@
  * the verification process may break
  * ***************************************************/
 
-//var bGround = require('fcc-express-bground');
-var bGround = require('./fcc');
+var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
 var app = express();
@@ -19,39 +18,12 @@ if (!process.env.DISABLE_XORIGIN) {
 				res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		}
 		next();
-	})
-	.use("/public", express.static(__dirname + '/public'))
-	.get("/", function(req, res) {
-		res.sendFile(__dirname + "/views/index.html");
-	})
-	.get("/json", function(req, res) {
-		res.json({
-			"message": (process.env.MESSAGE_STYLE === 'uppercase') ? "HELLO JSON" : "Hello json"
-		});
-	})
-	.get("/now", function(req, res, next) {
-		req.time = new Date().toString();
-		next();
-	}, function(req, res) {
-		// check if /now route has a middleware before the handler
-		// var stack = (myApp.parent._router && myApp.parent._router.stack) || [];
-		// var nowRoute = stack.filter((l) => {
-		// 	if (l.route) {
-		// 		return l.route.path === '/now';
-		// 	}
-		// 	return false;
-		// });
-		// myApp._router = myApp.parent._router;
-		res.json({
-			time: req.time
-		});
 	});
 }
 
 var port = process.env.PORT || 3000;
 //myApp._router = { stack: [...app._router.stack] };
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
-	//console.log(myApp._router.stack);
 	bGround.log('Node is listening on port '+ port + '...')
 });
 
