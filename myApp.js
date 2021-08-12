bada47d7e4ae2b1abe7c938dab49ef892a2a32a7
Will Forbes <wforbes87@gmail.com>
2021-08-12 07:05:19 -0700

    challenge 9 - Get Query Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -1,32 +1,40 @@
 var express = require('express');
 var app = express();
-app.use("/public", express.static(__dirname + '/public'))
-	.use(function(req, res, next) {
-		var logString = req.method + " " + req.path + " - " + req.ip;
-		console.log(logString);
-		next();
-	})
-	.get("/", function(req, res) {
-		res.sendFile(__dirname + "/views/index.html");
-	})
-	.get("/json", function(req, res) {
-		res.json({
-			"message": (process.env.MESSAGE_STYLE === 'uppercase') ? "HELLO JSON" : "Hello json"
-		});
-	})
-	.get("/now", function(req, res, next) {
-		req.time = new Date().toString();
-		next();
-	}, function(req, res) {
-		res.json({
-			time: req.time
-		});
-	})
-	.get("/:word/echo", function (req, res) {
-		res.json({ echo: req.params.word });
+app.use('/public', express.static(__dirname + '/public'))
+
+app.use(function(req, res, next) {
+	var logString = req.method + ' ' + req.path + ' - ' + req.ip;
+	console.log(logString);
+	next();
+});
+	
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', function(req, res) {
+	res.json({
+		'message': (process.env.MESSAGE_STYLE === 'uppercase') ? 'HELLO JSON' : 'Hello json'
 	});
+});
+	
+app.get('/now', function(req, res, next) {
+	req.time = new Date().toString();
+	next();
+}, function(req, res) {
+	res.json({
+		time: req.time
+	});
+});
 
+app.get('/:word/echo', function (req, res) {
+	res.json({ echo: req.params.word });
+});
 
+app.get('/name', (req, res) => {
+	res.json({ name: req.query.first + ' ' + req.query.last });
+});
+// left off at https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/get-query-parameter-input-from-the-client
 
 
 
